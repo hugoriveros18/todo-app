f16810b9cc3687c5afd1e671257be4b7e6ed3754
hugoriveros18 <hugoriverosfajardo@gmail.com>
2022-09-24 14:00:00 -0500

    Desktop update

--- src/TodoItem/TodoItem.js
+++ src/TodoItem/TodoItem.js
@@ -5,16 +5,6 @@ import iconCross from '../app-images/icon-cross.svg';
 import iconMoon from '../app-images/icon-moon.svg';
 
 function TodoItem(props) {
-    let checkedTodo = () => {
-        if(props.completed){
-            if(props.appTheme === iconMoon){
-                return "checkedLight";
-            } else {
-                return "checkedDark";
-            }
-        }
-        return "";
-    }
 
     return(
         <Draggable draggableId={props.text} index={props.index}>
@@ -29,7 +19,7 @@ function TodoItem(props) {
                         <input className={`checkbox ${props.completed && 'checkboxSelected'} ${props.appTheme === iconMoon && 'checkboxLight'}`} type="checkbox" onClick={() => {props.todoCompleted(props.text);}}/>
                     </div>
                     <div className='todoTextContainer'>
-                        <p className={`todoText ${checkedTodo()} ${props.appTheme === iconMoon && 'todoTextLight'}`}>{props.text}</p>
+                        <p className={`todoText ${(props.completed && props.appTheme === iconMoon) && 'checkedLight'} ${(props.completed && props.appTheme !== iconMoon) && 'checkedDark'} ${props.appTheme === iconMoon && 'todoTextLight'}`}>{props.text}</p>
                     </div>
                     <div className="crossContainer">
                         <img src={iconCross} alt='Cross' onClick={() => props.deleteTodo(props.text)}/>
